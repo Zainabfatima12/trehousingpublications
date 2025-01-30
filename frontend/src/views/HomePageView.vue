bb6e43d79c6be9c109d2333106fd507396f8ce69
SinghNamrata04 <156833188+SinghNamrata04@users.noreply.github.com>
2025-01-30 23:12:15 +0530

    completed mission vision section

--- frontend/src/views/HomePageView.vue
+++ frontend/src/views/HomePageView.vue
@@ -2,6 +2,7 @@
   <Header/>
   <HeroSec />
   <AboutUs />
+  <MissionVision/>
   <RefBook />
   <NewsLetter />
   <Footer/>
@@ -14,12 +15,14 @@ import AboutUs from "./components/Homepage/AboutUs.vue";
 import NewsLetter from "@/components/Homepage/NewsLetter.vue";
 import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
+import MissionVision from "@/components/MissionVision.vue";
 
 export default {
   name: "HomePageView",
   components: {
     Header,
     AboutUs,
+    MissionVision,
     HeroSec,
     RefBook,
     NewsLetter,
