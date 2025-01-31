f446cfc6be40665e588beef70de8af03f3f3892b
SinghNamrata04 <156833188+SinghNamrata04@users.noreply.github.com>
2025-01-31 21:40:23 +0530

    error solved

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,7 +19,6 @@ import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
 import MissionVision from "./components/MissionVision.vue";
 import DownloadApp from "./components/Homepage/DownloadApp.vue";
-=======
 import JobOpportunities from "@/components/Homepage/JobOpportunitiesPage.vue";
 
 
