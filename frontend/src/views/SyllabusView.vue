9b9f7a3bef42b846973ebce9625ecf4814f8648c
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-02-06 00:38:22 +0530

    Latest Event

--- frontend/src/views/SyllabusView.vue
+++ frontend/src/views/SyllabusView.vue
@@ -1,5 +1,6 @@
 <template>
   <Header />
+  <LatestEvent/>
   <SyllabusHigherSecondary/>
   <syllabusPdf />
   <Footer />
@@ -10,11 +11,13 @@ import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
 import syllabusPdf from "@/components/syllabus/syllabusPdf.vue";
 import SyllabusHigherSecondary from "@/components/syllabus/SyllabusHigherSecondary.vue";
+import LatestEvent from "@/components/syllabus/LatestEvent.vue";
 
 export default {
   name: "SyllabusView",
   components: {
     Header,
+    LatestEvent,
     SyllabusHigherSecondary,
     syllabusPdf,
     Footer,
