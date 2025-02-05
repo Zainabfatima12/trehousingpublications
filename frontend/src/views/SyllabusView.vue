c5214a0e856d835f0f2912374b99529f86cb222a
ankit-027 <ankit2kk4@gmail.com>
2025-02-05 18:56:47 +0530

    Adding Syllabus Comp Of PDF Section

--- frontend/src/views/SyllabusView.vue
+++ frontend/src/views/SyllabusView.vue
@@ -1,18 +1,19 @@
 <template>
   <Header />
-
+  <syllabusPdf />
   <Footer />
 </template>
 
 <script>
 import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
+import syllabusPdf from "@/components/syllabus/syllabusPdf.vue";
 
 export default {
   name: "SyllabusView",
   components: {
     Header,
-
+    syllabusPdf,
     Footer,
   },
 };
