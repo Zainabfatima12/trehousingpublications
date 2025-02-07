b7872555d7206b95f3d41012ca4ddf391f2e8654
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-02-08 00:30:18 +0530

    Notification panel
    1.Create a folder in components named PYQs and a file NotificationPanel.vue inside it.
    2.Create a file PYQPageView.vue and then set URLs in index.js.

---
+++ frontend/src/views/PYQPageView.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header />
+  <Notification/>
+
+  
+
+
+ 
+  <Footer />
+</template>
+
+<script>
+import Header from "@/components/Homepage/HeaderSec.vue";
+import Footer from "@/components/Homepage/FooterSec.vue";
+import Notification from "@/components/PYQs/NotificationPanel.vue";
+
+
+
+
+export default {
+  name: "SyllabusView",
+  components: {
+    Header,
+    Notification,
+
+  
+    Footer,
+  },
+};
+</script>
+
+<style></style>
