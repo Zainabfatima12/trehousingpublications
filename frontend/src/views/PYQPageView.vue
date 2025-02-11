82331f148e60c1a89b3b61f7146bef46a215eb1d
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-02-11 21:43:34 +0530

    Update PYQPageView Page
    Update PYQPageView

--- frontend/src/views/PYQPageView.vue
+++ frontend/src/views/PYQPageView.vue
@@ -1,7 +1,9 @@
 <template>
   <Header />
   <Notification />
+  <PyqRightSection/>
   <TextTable/>
+  <BiharSyllabusComponent/>
 
   <PyqSyPdf />
   <Footer />
@@ -10,9 +12,13 @@
 <script>
 import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
-import Notification from "@/components/PYQs/NotificationPanel.vue";
-import PyqSyPdf from "@/components/PYQs/PyqSyPdf.vue";
+import Notification from "@/components/PYQ/NotificationPanel.vue";
+import PyqSyPdf from "@/components/PYQ/PyqSyPdf.vue";
 import TextTable from "@/components/PYQ/TextTable.vue";
+
+import PyqRightSection from "@/components/PYQ/PyqRightSection.vue";
+
+import BiharSyllabusComponent from "@/components/PYQ/BiharSyllabusComponent.vue";
 
 export default {
   name: "SyllabusView",
