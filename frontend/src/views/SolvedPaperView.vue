5c35c82271b9d30c6b7b74953f8b7729ecd4a7c5
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-02-17 15:17:40 +0530

    FAQs
    Created SolvedPaper folder in component
    Created Solvedpaperview file in Vues

---
+++ frontend/src/views/SolvedPaperView.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header />
+  <faq/>
+
+
+  <Footer />
+</template>
+
+<script>
+import Header from "@/components/Homepage/HeaderSec.vue";
+import Footer from "@/components/Homepage/FooterSec.vue";
+import faq from'@/components/SolvedPaper/FAQ.vue';
+
+export default {
+  name: "SolvedPaperView",
+  components: {
+    Header,
+    faq,
+   
+
+    Footer,
+  },
+};
+</script>
+
+<style></style>
