606b3516f060f25d4c9584aac6dc5c57f8ae2544
Udit-Singh-34567 <123153718+Udit-Singh-34567@users.noreply.github.com>
2025-02-27 15:43:24 +0530

    popular test series

--- frontend/src/views/TestSeriesView.vue
+++ frontend/src/views/TestSeriesView.vue
@@ -1,7 +1,6 @@
 <template>
     <Header />
-
-
+    <PopularTest />
     <SeriesCategories />
     <Footer />
 </template>
@@ -9,15 +8,15 @@
 <script>
 import Header from "@/components/Homepage/HeaderSec.vue";
 import Footer from "@/components/Homepage/FooterSec.vue";
-
+import PopularTest from '@/components/TestSeries/PopularTest.vue';
 import SeriesCategories from '@/components/TestSeries/SeriesCategories.vue';
 
 export default{
     name : 'TestSeriesView',
     components : {
         Header,
+        PopularTest,
         SeriesCategories,
-
         Footer,
     }
 }
